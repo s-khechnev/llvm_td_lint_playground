6f0dbbb53faaa9e9b8f2720b68a910bd6efbabe6
Semyon Khechnev <hechnev@gmail.com>
2025-05-06 22:18:29 +0300

    Polishing report format

--- src/comparator.ml
+++ src/comparator.ml
@@ -4,17 +4,7 @@ open Utils
 let () =
   Llvm_info.llvm_info |> Hashtbl.to_seq |> List.of_seq
   |> List.sort (fun (m1, _) (m2, _) -> compare m1 m2)
-  |> List.iter (fun (iname, (llvm_instr : Instruction.t)) ->
-         let f opers xs ppf =
-           xs
-           |> List.map (fun oper ->
-                  match List.find_index (String.equal oper) opers with
-                  | Some i -> (oper, i)
-                  | None ->
-                      printfn ppf iname oper;
-                      (oper, -1))
-           |> List.sort (fun (_, i1) (_, i2) -> compare i2 i1)
-         in
+  |> List.iter (fun (iname, llvm_instr) ->
          let {
            mnemonic = llvm_mnemonic;
            operands = llvm_opers;
@@ -31,7 +21,6 @@ let () =
          in
          match Sail_info.find_opt llvm_mnemonic with
          | Some sail_instr ->
-             printfn "Report: %s" iname;
              let {
                mnemonic = _;
                operands = sail_opers;
@@ -40,6 +29,8 @@ let () =
              } : Instruction.t =
                sail_instr
              in
+
+             printfn "Report: %s" iname;
 
              let lst_str ppf out =
                Format.pp_print_list
@@ -50,40 +41,42 @@ let () =
              printfn "llvm: %s %a" iname lst_str llvm_opers;
              printfn "sail: %s %a" iname lst_str sail_opers;
 
-             let llvm_ins = f llvm_opers llvm_ins "llvm %s: %s - implicit in" in
-             let llvm_outs =
-               f llvm_opers llvm_outs "llvm %s: %s - implicit out"
+             let f opers xs ppf =
+               xs
+               |> List.map (fun oper ->
+                      match List.find_index (String.equal oper) opers with
+                      | Some i -> (oper, i)
+                      | None ->
+                          printfn ppf oper;
+                          (oper, -1))
+               |> List.sort (fun (_, i1) (_, i2) -> compare i2 i1)
              in
-             let sail_ins = f sail_opers sail_ins "sail %s: %s - implicit in" in
-             let sail_outs =
-               f sail_opers sail_outs "sail %s: %s - implicit out"
-             in
+             let llvm_ins = f llvm_opers llvm_ins "llvm: %s - implicit in" in
+             let llvm_outs = f llvm_opers llvm_outs "llvm: %s - implicit out" in
+             let sail_ins = f sail_opers sail_ins "sail: %s - implicit in" in
+             let sail_outs = f sail_opers sail_outs "sail: %s - implicit out" in
 
              if List.length sail_opers <> List.length llvm_opers then
-               printfn "%s: Different number of operands" iname
+               printfn "Different number of operands"
              else (
                (try
                   List.iter2
                     (fun (_, llvm_i) (_, sail_i) ->
                       if llvm_i <> sail_i then
-                        printfn "%s: Different outs (%d, %d)" iname llvm_i
-                          sail_i)
+                        printfn "Different outs (%d, %d)" llvm_i sail_i)
                     llvm_outs sail_outs
                 with
-               | Invalid_argument _ ->
-                   printfn "%s: Different number of outs" iname
+               | Invalid_argument _ -> printfn "Different number of outs"
                | _ -> ());
 
                try
                  List.iter2
                    (fun (_, llvm_i) (_, sail_i) ->
                      if llvm_i <> sail_i then
-                       printfn "%s: Different inputs (%d, %d)" iname llvm_i
-                         sail_i)
+                       printfn "Different inputs (%d, %d)" llvm_i sail_i)
                    llvm_ins sail_ins
                with
-               | Invalid_argument _ ->
-                   printfn "%s: Different number of inputs" iname
+               | Invalid_argument _ -> printfn "Different number of inputs"
                | _ -> ());
 
              let f lst =
